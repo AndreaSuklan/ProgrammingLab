94e4a5b2f007d4f0950f6d34aa64b9026b586985
repl.it user <replituser@example.com>
2020-11-15 16:27:24 +0000

    Lettura_testo

--- Sommatorialista.py
+++ Sommatorialista.py
@@ -1,3 +1,19 @@
-x=1,2,3,4
-y=sum(x)
-print(y)
+#Apro il file
+file_origin=open("Shampoo.txt","r")
+#Leggo il contenuto
+file=file_origin.readlines()
+#Inizializzo la lista dove metterò i valori
+valori=[]
+#Lehho il file linea per linea
+for linea in file:
+    elemento=linea.split(',')
+    #Sommo solo se i dati hanno valore effettivo
+    if elemento[0]!='Date':
+        #Separo data e numero
+        data=elemento[0]
+        numero=elemento[1]
+        #Assegno i valori ad un array
+        valori.append(float(numero))
+#Stampo i valori e la loro somma
+print(valori)
+print(sum(valori))
