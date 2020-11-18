e0f9d9b21d826461d474d090ed75d9c0d8d315bb
repl.it user <replituser@example.com>
2020-11-18 17:01:05 +0000

    ConfrontoClassi

--- ClassePersona.py
+++ ClassePersona.py
@@ -47,14 +47,39 @@ class Studente(Persona):
         #Anche le sue funzioni
         Persona.bonjour(self)
         for i in range(len(self.corso)):
-            print('> Docente del corso:', self.corso[i])
+            print('> Studente del corso:', self.corso[i])
         print("\n")
 pass
+#Definisco la funzione
+def docente_copre_studente(docente,studente):
+    k=0
+    #Un pò di interfaccia utente
+    print("\nProfessore che esamino\n")
+    print("---------o-------\n")
+    docente.bonjour()
+    print("---------o-------\n")
+    print("Studente che esamino\n")
+    studente.bonjour()
+    print("---------o-------\n")
+    print("Ricerca in corso...\n")
+    print("---------o-------\n")
+    for i in range(len(docente.corso)):
+        for j in range(len(studente.corso)):
+            if (docente.corso[i] == studente.corso[j]):
+                print(docente.corso[i],"== Vero\n")
+                k=k+1
+    print("---------o-------\n")
+    if (k == len(studente.corso)):
+        print("Il professore {} {} copre tutti i corsi di {} {}\n".format(docente.nome,docente.cognome,studente.nome,studente.cognome))
+    else:
+        print("Il professore {} {} non copre tutti i corsi di {} {}\n".format(docente.nome,docente.cognome,studente.nome,studente.cognome))
+    print("---------o-------\n")
+pass
+
 Gianni=Docente("Gianni","Micheli",37,["Informatica","Analisi","Laboratorio"])
-Gianni.bonjour()
+Lollo=Docente("Lollo","Pollo",12,["Informatica","Analisi","Latino"])
 Giorgio=Docente("Giorgio","Milano",57,["Latino","Analisi","Laboratorio"])
-Giorgio.bonjour()
 Marco=TecnicoAmministrativo("Marco","Maraldo",30,["H3","C9"])
-Marco.bonjour()
 Poco=Studente("Poco","Furbo",57,["Informatica","Analisi","Latino"])
-Poco.bonjour()
+docente_copre_studente(Gianni,Poco)
+docente_copre_studente(Lollo,Poco)
