8a3c8d67b81df76c3f71a2ee24fc6ab46679e8e2
repl.it user <replituser@example.com>
2020-11-18 15:40:53 +0000

    AggiuntoEtà

--- ClassePersona.py
+++ ClassePersona.py
@@ -1,19 +1,23 @@
 #Definisco la classe Persona
 class Persona:
     #Inizializzo le variabili(Attributi)
-    def __init__(self,ruolo,nome,cognome):
+    def __init__(self,ruolo,nome,cognome,age,):
         self.ruolo=ruolo
         self.nome=nome
         self.cognome=cognome
+        self.age=age
     #Definisco le funzioni interne(Metodi)
     def bonjour(self):
-        print(self.ruolo,': ',self.nome,self.cognome)
+        if (self.age > 40):
+            print(self.ruolo,':',self.nome,self.cognome,"(anni >40)",)
+        else:
+            print(self.ruolo,':',self.nome,self.cognome,'(anni {})'.format(self.age))
 pass
 #Definisco sottoclasse Docente
 class Docente(Persona):
-    def __init__(self, nome, cognome, corso):
+    def __init__(self, nome, cognome,age,corso):
         #Posso utilizzare i dati della superclasse Persona
-        super().__init__('Docente UNITS', nome, cognome)
+        super().__init__('Docente UNITS', nome, cognome,age)
         self.corso=corso
     def bonjour(self): 
         #Anche le sue funzioni
@@ -22,8 +26,8 @@ class Docente(Persona):
 pass
 #Definisco sottoclasse Tecnico
 class TecnicoAmministrativo(Persona):
-    def __init__(self,nome,cognome,edificio):
-        super().__init__('Tecnico UNITS:',nome,cognome)
+    def __init__(self,nome,cognome,age,edificio):
+        super().__init__('Tecnico UNITS:',nome,cognome,age)
         self.edificio=edificio
     def bonjour(self):
         Persona.bonjour(self)
@@ -31,18 +35,18 @@ class TecnicoAmministrativo(Persona):
 pass
 #Definisco sottoclasse Studente
 class Studente(Persona):
-    def __init__(self, nome, cognome, corso):
+    def __init__(self, nome, cognome,age,corso):
         #Posso utilizzare i dati della superclasse Persona
-        super().__init__('Studente UNITS', nome, cognome)
+        super().__init__('Studente UNITS', nome, cognome,age)
         self.corso=corso
     def bonjour(self): 
         #Anche le sue funzioni
         Persona.bonjour(self)
         print('> Studente del corso(i):', self.corso,"\n")
 pass
-Gianni=Docente("Gianni","Micheli",["Informatica","Analisi","Laboratorio"])
+Gianni=Docente("Gianni","Micheli",37,["Informatica","Analisi","Laboratorio"])
 Gianni.bonjour()
-Marco=TecnicoAmministrativo("Marco","Maraldo",["H3","C9"])
+Marco=TecnicoAmministrativo("Marco","Maraldo",30,["H3","C9"])
 Marco.bonjour()
-Poco=Studente("Poco","Furbo",["Informatica","Analisi","Latino"])
+Poco=Studente("Poco","Furbo",57,["Informatica","Analisi","Latino"])
 Poco.bonjour()
