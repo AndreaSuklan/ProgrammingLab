4c12bcdef39df30b82d8db1154442b545a08e39e
repl.it user <replituser@example.com>
2020-11-20 15:26:10 +0000

    RilevErrore

--- Sommatorialista.py
+++ Sommatorialista.py
@@ -4,16 +4,24 @@ file_origin=open("Shampoo.txt","r")
 file=file_origin.readlines()
 #Inizializzo la lista dove metterò i valori
 valori=[]
-#Lehho il file linea per linea
+i=1
+#Leggo il file linea per linea
 for linea in file:
     elemento=linea.split(',')
-    #Sommo solo se i dati hanno valore effettivo
-    if elemento[0]!='Date':
+        #Sommo solo se i dati hanno valore effettivo
+    if (elemento[0]!='Date'):
         #Separo data e numero
         data=elemento[0]
         numero=elemento[1]
         #Assegno i valori ad un array
-        valori.append(float(numero))
+        try:
+            valori.append(float(numero))
+        except TypeError:
+            print("La riga {} non contiene valori numerici ma {}".format(i,linea))
+        except:
+            print("La riga {} contiene un errore sconosciuto; ovvero {}".format(i,linea))
+    i=i+1
+
 #Stampo i valori e la loro somma
-print(valori)
-print(sum(valori))
+print("I valori sono:\n",valori,"\n")
+print("La sommatoria equivale a ",sum(valori))
